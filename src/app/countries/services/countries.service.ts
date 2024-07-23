1df27cba88b6ebeb97e4cb89ace2c9dcfe4e87e1
Victor Rodriguez Garcia <vrodriguez@opentrends.net>
2024-07-23 18:14:31 +0200

    save and load to local storage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { catchError, delay, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CacheStore } from '../interfaces/cache-store.interface';
@@ -17,7 +17,18 @@ export class CountriesService {
     byRegion: { region: '', countries: [] },
   };
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.loadCacheFromLocalStorage();
+  }
+
+  private saveCacheToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadCacheFromLocalStorage() {
+    if (!localStorage.getItem('cacheStore')) return;
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  }
 
   searchCountryByAlphCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
@@ -32,19 +43,28 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     return this.invokeUrl(`${this.apiUrl}/capital/${term}`).pipe(
-      tap((countries) => (this.cacheStore.byCapital = { term, countries }))
+      tap((countries) => (this.cacheStore.byCapital = { term, countries })),
+      tap(() => {
+        this.saveCacheToLocalStorage();
+      })
     );
   }
 
   searchCountry(term: string): Observable<Country[]> {
     return this.invokeUrl(`${this.apiUrl}/name/${term}`).pipe(
-      tap((countries) => (this.cacheStore.byCountry = { term, countries }))
+      tap((countries) => (this.cacheStore.byCountry = { term, countries })),
+      tap(() => {
+        this.saveCacheToLocalStorage();
+      })
     );
   }
 
   searchRegion(region: Region): Observable<Country[]> {
     return this.invokeUrl(`${this.apiUrl}/region/${region}`).pipe(
-      tap((countries) => (this.cacheStore.byRegion = { region, countries }))
+      tap((countries) => (this.cacheStore.byRegion = { region, countries })),
+      tap(() => {
+        this.saveCacheToLocalStorage();
+      })
     );
   }
 
